5dfcc6935c5dd3995f7b48dcbcae6d1f2498fc99
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2024-04-16 11:35:57 +0200

    Classe d'administration pour les utilisateurs

---
+++ project_em/users/admin.py
@@ -0,0 +1,61 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.forms import CustomUserChangeForm, CustomUserCreationForm
+from users.models import CustomUser
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": (
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "date_of_birth")}
+        ),
+        ("Permissions", {"fields": (
+            "is_staff",
+            "is_active",
+            "groups",
+            "user_permissions")}
+        ),
+    )
+    add_fieldsets = (
+        ( None, {"fields": (
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+            "date_of_birth",
+            "is_staff",
+            "is_active",
+            "groups",
+            "user_permissions")}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+# Inclusion de User dans l'interface d'administration
+admin.site.register(CustomUser, CustomUserAdmin)
+
